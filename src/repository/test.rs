6258a6ef1ab1074e2128aad675d07696417cb530
Viacheslav Matrizaev <matrizaev@gmail.com>
2025-07-07 12:18:42 +0300

    Fix typos and address field in repo

--- src/repository/test.rs
+++ src/repository/test.rs
@@ -18,7 +18,7 @@ impl ClientRepository for TestClientRepository {
             name: new_client.name.clone(),
             email: new_client.email.clone(),
             phone: new_client.phone.clone(),
-            address: new_client.phone.clone(),
+            address: new_client.address.clone(),
             ..Client::default()
         };
 
@@ -66,7 +66,7 @@ impl ClientRepository for TestClientRepository {
             name: updates.name.clone(),
             email: updates.email.clone(),
             phone: updates.phone.clone(),
-            address: updates.phone.clone(),
+            address: updates.address.clone(),
             ..Client::default()
         };
 
