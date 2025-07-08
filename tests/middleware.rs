075e99ad875d95096396ed163d26bd695a9bc292
Viacheslav Matrizaev <matrizaev@gmail.com>
2025-07-08 08:38:51 +0000

    fixed middleware test

--- tests/middleware.rs
+++ tests/middleware.rs
@@ -4,13 +4,19 @@ use actix_web::{
     test, web,
 };
 
-use pushkind_crm::middleware::RedirectUnauthorized;
+use pushkind_crm::{middleware::RedirectUnauthorized, models::config::ServerConfig};
 
 #[actix_web::test]
 async fn redirects_unauthorized_to_signin() {
+    let server_config = ServerConfig {
+        secret: "secret".to_string(),
+        auth_service_url: "http://auth.test.me/".to_string(),
+    };
+
     let app = test::init_service(
         App::new()
             .wrap(RedirectUnauthorized)
+            .app_data(web::Data::new(server_config.clone()))
             .default_service(web::to(|| async { HttpResponse::Unauthorized().finish() })),
     )
     .await;
@@ -21,15 +27,20 @@ async fn redirects_unauthorized_to_signin() {
     assert_eq!(resp.status(), StatusCode::SEE_OTHER);
     assert_eq!(
         resp.headers().get(header::LOCATION).unwrap(),
-        "/auth/signin"
+        "http://auth.test.me/"
     );
 }
 
 #[actix_web::test]
 async fn success_response_passes_through() {
+    let server_config = ServerConfig {
+        secret: "secret".to_string(),
+        auth_service_url: "http://auth.test.me/".to_string(),
+    };
     let app = test::init_service(
         App::new()
             .wrap(RedirectUnauthorized)
+            .app_data(web::Data::new(server_config.clone()))
             .default_service(web::to(|| async { HttpResponse::Ok().finish() })),
     )
     .await;
