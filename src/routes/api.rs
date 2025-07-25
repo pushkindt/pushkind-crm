f8f8d85f1da1aa4b9d273ae2029c96e11e05d498
Viacheslav Matrizaev <matrizaev@gmail.com>
2025-07-25 12:12:46 +0000

    Added pagination support for api clients

--- src/routes/api.rs
+++ src/routes/api.rs
@@ -1,16 +1,16 @@
 use actix_web::{HttpResponse, Responder, get, web};
-use log::error;
 use pushkind_common::db::DbPool;
 use pushkind_common::models::auth::AuthenticatedUser;
-use pushkind_common::routes::ensure_role;
+use pushkind_common::routes::{DEFAULT_ITEMS_PER_PAGE, ensure_role};
 use serde::Deserialize;
 
 use crate::repository::client::DieselClientRepository;
-use crate::repository::{ClientReader, ClientSearchQuery};
+use crate::repository::{ClientListQuery, ClientReader, ClientSearchQuery};
 
 #[derive(Deserialize)]
 struct ApiV1ClientsQueryParams {
-    query: String,
+    query: Option<String>,
+    page: Option<usize>,
 }
 
 #[get("/v1/clients")]
@@ -24,11 +24,34 @@ pub async fn api_v1_clients(
     }
     let repo = DieselClientRepository::new(&pool);
 
-    match repo.search(ClientSearchQuery::new(user.hub_id, &params.query)) {
-        Ok((_total, clients)) => HttpResponse::Ok().json(clients),
-        Err(e) => {
-            error!("Failed to list clients: {e}");
-            HttpResponse::InternalServerError().finish()
+    match &params.query {
+        Some(query) if !query.is_empty() => {
+            let mut search_params = ClientSearchQuery::new(user.hub_id, query);
+            if let Some(page) = params.page {
+                search_params = search_params.paginate(page, DEFAULT_ITEMS_PER_PAGE);
+            }
+
+            match repo.search(search_params) {
+                Ok((_total, clients)) => HttpResponse::Ok().json(clients),
+                Err(e) => {
+                    log::error!("Failed to list clients: {e}");
+                    HttpResponse::InternalServerError().finish()
+                }
+            }
+        }
+        _ => {
+            let mut list_params = ClientListQuery::new(user.hub_id);
+            if let Some(page) = params.page {
+                list_params = list_params.paginate(page, DEFAULT_ITEMS_PER_PAGE);
+            }
+
+            match repo.list(list_params) {
+                Ok((_total, clients)) => HttpResponse::Ok().json(clients),
+                Err(e) => {
+                    log::error!("Failed to list clients: {e}");
+                    HttpResponse::InternalServerError().finish()
+                }
+            }
         }
     }
 }
